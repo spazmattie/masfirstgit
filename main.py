e95a1dcc4f37eeaf2a7feb534ced7529e84088d5
spazmattie <spazmattie@gmail.com>
2020-07-28 14:49:05 -0400

    Worked on the navigation bar a little

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index.html")
 def home():
     return render_template("index.html")
+
+@app.route("/graph")
+def graph():
+    return render_template("graph.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
