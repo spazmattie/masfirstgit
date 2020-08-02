39d1d8ca19331c05b6caf815b1e0df3e0c46a235
s3ahmed <bathamese101@gmail.com>
2020-08-02 12:29:22 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
 import psycopg2 as ps
 
 df = pd.read_csv('Algebra 2 & Trig #2.csv')
 
 print (df.head())
 
-=======
 import psycopg2 as ps 
->>>>>>> e9eae7a04a852d9c82438742fd748d30311fb7ff
