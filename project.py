2ccb62ccf5b7595c2dcb1fd547d48699b57f1fe8
spazmattie <spazmattie@gmail.com>
2020-08-03 11:05:51 -0400

    Pulling repository

--- project.py
+++ project.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import psycopg2 as ps
+import plotly.express as px4
 
 df = pd.read_excel('Chemistry.xlsx')
 
 print (df.head())
 
+plt.plot([1, 2, 3, 4])
+plt.ylabel('list of numbers')
+plt.show()
 
